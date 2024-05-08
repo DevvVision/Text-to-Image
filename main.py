82aaf3d6e7b562e3794312d1e4c8404b930c301f
HarshitSrivastava <167855220+Harshit-Srivastava25@users.noreply.github.com>
2024-05-09 01:07:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-#from dotenv import find_dotenv,load_dotenv
 import streamlit as st
-# from transformers import pipeline
-# Use a pipeline as a high-level helper
-# load_dotenv(find_dotenv())
-# from transformers import pipeline
-# def imagetotext(url):
-#     pipe = pipeline("image-to-text", model="Salesforce/blip-image-captioning-large")
-#     text = pipe(url)
-#     return text
-# def texttoimage(txxt):
-#     pipeline = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0")
-
-# print(imagetotext("photo.jpg"))
 st.write("# **Anime**")
 st.write("# Text to Image generator")
 import requests
 
 API_URL = "https://api-inference.huggingface.co/models/cagliostrolab/animagine-xl-3.1"
-headers = {"Authorization": "Bearer xxxxxxxxxxxxxxxxxxxxxxx"}
+headers = {"Authorization": "Bearer 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"}
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
@@ -34,4 +21,3 @@ if(check==True):
     from PIL import Image
     image = Image.open(io.BytesIO(image_bytes))
     st.image(image)
-# load_dotenv(find_dotenv())
