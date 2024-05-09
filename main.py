351d3bd817d1bea70028e1cf3d45688301c1e43b
HarshitSrivastava <167855220+Harshit-Srivastava25@users.noreply.github.com>
2024-05-09 11:42:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ st.write("# Text to Image generator")
 import requests
 
 API_URL = "https://api-inference.huggingface.co/models/cagliostrolab/animagine-xl-3.1"
-headers = {"Authorization": "Bearer "}
+headers = {"Authorization": "Bearer 9e10a3ea36b4872f2c60378e82e8600a"}
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
